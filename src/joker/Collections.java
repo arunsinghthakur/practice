0fc2529497936df32b162f416062d026f5d3ec7f
arunsingh <arunsingh@Arun3735.Nagarro.local>
2018-07-20 16:07:16 +0530

    cc

---
+++ src/joker/Collections.java
@@ -0,0 +1,28 @@
+package joker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		Set<String> set = new HashSet<>();
+		Map<String, String> map = new HashMap<>();
+		Set<String> tset = new TreeSet<>();
+		Map<String, String> tmap = new TreeMap<>();
+
+		list.add(null);
+		set.add(null);
+		map.put(null, null);
+		tset.add(null);
+		tmap.put(null, null);
+	}
+
+}
