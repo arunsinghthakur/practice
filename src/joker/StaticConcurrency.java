511667170fd9f0ad884be5940fdaffdb5dc97ed9
ArunSingh <ArunSingh@DESKTOP-B38ID3L.lan>
2019-03-24 19:51:06 +0530

    test

--- src/joker/StaticConcurrency.java
+++ src/joker/StaticConcurrency.java
@@ -1,35 +1,37 @@
 package joker;
 
-public class StaticConcurrency implements Runnable {
-	
-	private /*static*/ String STATIC_OBJ = "JOKER";
-	private static Dummy STATIC_OBJ2 = new Dummy();
-	
+public class StaticConcurrency {
+	private static int count = 0;
 	public static void main(String[] args) {
-		StaticConcurrency obj1 = new StaticConcurrency();
-		StaticConcurrency obj2 = new StaticConcurrency();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (StaticConcurrency.class) {
+					Thread.currentThread();
+					System.out.println(Thread.currentThread().getName() + "-" + count++);
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}, "static thread").start();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (this) {
+					System.out.println(Thread.currentThread().getName() + "-" + count++);
+				}
+			}
+		}, "instance thread").start();
 		
-		new Thread(obj1).start();
-		new Thread(obj2).start();
+		System.out.println("main thread - " + count++);
 	}
 
-	@Override
-	public void run() {
-		try {
-			task();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void task() throws InterruptedException {
-		synchronized (STATIC_OBJ2) {
-			System.out.println(Thread.currentThread().currentThread().getName());
-			Thread.sleep(5000);
-		}
-	}
-
-}
-class Dummy{
-	
 }
